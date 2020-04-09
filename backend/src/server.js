9cb87d12d9a00a62afc8355dd3e710b6c6021130
Daniel Cunha <danielcunha54@gmail.com>
2020-04-08 21:11:23 -0300

    Finalizado o front-end

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,13 +2,17 @@ require("dotenv").config();
 const chalk = require("chalk");
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
+const path = require("path");
 
 const routes = require("./routes");
 
 const app = express();
 const port = process.env.PORT || 3333;
 
+app.use(cors());
 app.use(express.json());
+app.use("/files", express.static(path.resolve(__dirname, "..", "uploads")));
 app.use(routes);
 
 mongoose.connect(process.env.MONGODB_URL, {
